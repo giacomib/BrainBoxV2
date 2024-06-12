688b486340a00e71fcedc6ec1ee0ec6c3068cdd4
Giacomo Motta <g.motta22@campus.unimib.it>
2024-06-12 13:08:26 +0200

    Added Note java class

---
+++ app/src/main/java/com/jackflow/brainboxv2/Note.java
@@ -0,0 +1,38 @@
+package com.jackflow.brainboxv2;
+
+public class Note {
+
+    private String title;
+
+    private String text;
+
+    public Note() {
+    }
+
+    /**
+     * a simple class representing a note
+     * @param title title of the note
+     * @param text actual text of the note
+     */
+    public Note(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
